797ef0af0097dda6b29a49f54cd05d08e93d185f
seyxxn <lucy9480@gmail.com>
2022-01-08 22:11:45 +0900

    store main page ssua

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,27 @@
+import styles from "pages/Home.module.css";
+import main_ssua1 from "assets/main_ssua1.png";
+import main_ssua2 from "assets/main_ssua2.png";
+import main_ssua3 from "assets/main_ssua3.png";
+
 function Home(){
     return (
-        <div>Home_Carousel</div>
+        <div id={styles.home}>
+            <img 
+            className={styles.mainSsua1}
+            src={main_ssua1}
+            alt="main_ssua1" 
+            />
+            <img 
+            className={styles.mainSsua2}
+            src={main_ssua2}
+            alt="main_ssua2" 
+            />
+            <img 
+            className={styles.mainSsua3}
+            src={main_ssua3}
+            alt="main_ssua3" 
+            />
+        </div>
     );
 }
 
