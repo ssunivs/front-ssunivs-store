b1a9f309bfeb7ac2dbcf912d4e403927f3043421
hjch0211 <gkswocks123@gmail.com>
2022-01-11 15:45:10 +0900

    headTitle.css update

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,6 +1,12 @@
+import headTitle from "pages/headTitle.module.css";
+
 function MyPage(){
     return (
-        <div>MyPage</div>
+        <div>
+            <h1 className={headTitle.head}>My Page</h1>
+
+            <div>ddd</div>
+        </div>
     );
 }
 
