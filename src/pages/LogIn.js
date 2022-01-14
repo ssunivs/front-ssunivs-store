c51f9aa5ee90d4b225992e9258926b22b151942c
seyxxn <lucy9480@gmail.com>
2022-01-14 14:19:27 +0900

    LogIn page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,25 @@
+import styles from "pages/LogIn.module.css"
+
 function LogIn(){
     return (
         <div>
             <h1>Log In</h1>
+            <form id={styles.userLogInForm}>
+                <input id ={styles.userID}
+                type="text" placeholder="ID" value="" autofocus></input>
+                <br></br>
+                <input id ={styles.userPW}
+                type="text" placeholder="Password" value="" autofocus></input>
+                <p id={styles.findPW}> 비밀번호찾기</p>
+                <input id = {styles.logInBtn}
+                type="submit" value="로그인"></input>
+                <br></br>
+                <input id = {styles.signUpBtn}
+                type="button" value="회원가입"></input>
+                <br></br>
+                <input id = {styles.logInGoogleBtn}
+                type="button" value="구글 로그인"></input>
+            </form>
         </div>
     );
 }
