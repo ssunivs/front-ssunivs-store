856b0f06ed554ed49e7b9e23d732119ab71e2e7d
hjch0211 <gkswocks123@gmail.com>
2022-01-14 11:16:09 +0900

    Menu transition Update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,6 +18,7 @@ function Menu(){
 
     useEffect((path) => {
         path = location.pathname;
+        setOpen(false);
     }, [location]);
 
     return (
@@ -40,22 +41,28 @@ function Menu(){
             <ul className={styles.menuList}>
                 <li>ABOUT US</li>
                 <ul className={styles.detailLists}>
-                <Link to="/AboutSsua"><li className={styles.list}>ssua</li></Link>
-                <Link to="/AboutSsunivers"><li className={styles.list}>ssuniverse</li></Link>
+                <Link to="/AboutSsua"><li className={styles.list}
+                                        style={{color: (path==="/AboutSsua")? "#A5D1FC" : ""}}>ssua</li></Link>
+                <Link to="/AboutSsunivers"><li className={styles.list}
+                                            style={{color: (path==="/AboutSsunivers")? "#A5D1FC" : ""}}>ssuniverse</li></Link>
                     <br></br>
                 </ul>
 
                 <li>STORE</li>
                 <ul className={styles.detailLists}>
-                <Link to="/MainStore"><li className={styles.list}>All</li></Link>
-                <Link to="/Reservation"><li className={styles.list}>Reservation</li></Link>
+                <Link to="/MainStore"><li className={styles.list}
+                                        style={{color: (path==="/MainStore")? "#A5D1FC" : ""}}>All</li></Link>
+                <Link to="/Reservation"><li className={styles.list}
+                                        style={{color: (path==="/Reservation")? "#A5D1FC" : ""}}>Reservation</li></Link>
                     <br></br>
                 </ul>
 
                 <li>BOARD</li>
                 <ul className={styles.detailLists}>
-                <Link to="/Notice"><li className={styles.list}>Notice</li></Link>
-                <Link to="/FAQ"><li className={styles.list}>FAQ</li></Link>
+                <Link to="/Notice"><li className={styles.list}
+                                       style={{color: (path==="/Notice")? "#A5D1FC" : ""}}>Notice</li></Link>
+                <Link to="/FAQ"><li className={styles.list}
+                                       style={{color: (path==="/FAQ")? "#A5D1FC" : ""}}>FAQ</li></Link>
                     <br></br>
                 </ul>
 
@@ -72,9 +79,12 @@ function Menu(){
             <SocialLinks size="30px" color="white"/>
 
                 <ul className={styles.user}>
-                    <Link to="/LogIn"><li className={styles.list}>LOG IN</li></Link>
-                    <Link to="/MyPage"><li className={styles.list}>MY PAGE</li></Link>
-                    <Link to="Cart"><li className={styles.list}>CART</li></Link>    
+                    <Link to="/LogIn"><li className={styles.list}
+                                          style={{color: (path==="/LogIn")? "#A5D1FC" : ""}}>LOG IN</li></Link>
+                    <Link to="/MyPage"><li className={styles.list}
+                                          style={{color: (path==="/MyPage")? "#A5D1FC" : ""}}>MY PAGE</li></Link>
+                    <Link to="Cart"><li className={styles.list}
+                                          style={{color: (path==="/Cart")? "#A5D1FC" : ""}}>CART</li></Link>    
                 </ul>
             </div>
         </div>
