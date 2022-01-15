4541f9249aa1a3fe77abdaafa1dd333163bbb16f
hjch0211 <gkswocks123@gmail.com>
2022-01-15 14:18:45 +0900

    드디어... 스토리지 값이 바뀐다...

--- src/redux/logInOut/logInOut.js
+++ src/redux/logInOut/logInOut.js
@@ -1,16 +1,16 @@
 import { connect } from "react-redux";
 import { setLogIn, setLogOut } from "./actions";
 
-const LogInOut = ({ id, logInState}) => {
+const LogInOut = ({ id, logInState, setLogIn, setLogOut}) => {
     return(
         <div>
             <button id = {id}
                    style={{display: (logInState)? "none": ""}}
-                   onClick={() => setLogIn}>(임시) 바로 로그인
+                   onClick={setLogIn}>(임시) 바로 로그인
                                             {logInState}</button>
             <button id = {id}
                    style={{display: (logInState)? "": "none"}}
-                   onClick={() => setLogOut}>(임시) 바로 로그아웃</button>    
+                   onClick={setLogOut}>(임시) 바로 로그아웃</button>    
         </div>
     );
 };
@@ -33,8 +33,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch)=>{
     return {
-        setLogIn: ()=>dispatch(setLogIn),
-        setLogOut: ()=>dispatch(setLogOut)
+        setLogIn: ()=>dispatch(setLogIn()),
+        setLogOut: ()=>dispatch(setLogOut())
     };
 };
 
