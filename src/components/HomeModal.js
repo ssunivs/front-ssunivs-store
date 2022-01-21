546607ceaaf2a07ded93ebdce5bef10b805c331d
seyxxn <lucy9480@gmail.com>
2022-01-21 17:31:21 +0900

    임시 모달창(버튼눌러야함)

---
+++ src/components/HomeModal.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import img from "assets/coupon-main.png";
+import styles from "components/HomeModal.module.css";
+import {Link} from "react-router-dom";
+
+export default function HomeModal() {
+//   const [index, setIndex] = useState(0);
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const onClickOutside = (e) => {
+    if (e.target.localName !== "button") {
+      setDisplayModal(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("click", onClickOutside);
+    return () => window.removeEventListener("click", onClickOutside);
+  }, []);
+  return (
+    <div className="HomeModal">
+      <button onClick={() => setDisplayModal(true)}>모달창 버튼</button>
+      {displayModal && (
+        <div className={styles.modalImg}>
+            <Link to="/Notice.Event">
+            <img src={img} alt="쿠폰발급방법"/>
+            </Link> 
+        </div>
+      )}
+    </div>
+  );
+}
