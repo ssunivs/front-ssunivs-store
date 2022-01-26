26394b1ff0e4b852a94ad6e31368bff284cd1c4c
seyxxn <lucy9480@gmail.com>
2022-01-26 23:41:01 +0900

    방문수령예약 페이지 수정

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -6,7 +6,6 @@ import DatePicker from "react-datepicker";
 import ko from "date-fns/locale/ko";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from "pages/Reservation.module.css";
-import userForm from "pages/EditProfile.module.css";
 import setHours from "date-fns/setHours";
 import setMinutes from "date-fns/setMinutes";
 import getDay from "date-fns/getDay";
@@ -40,76 +39,83 @@ function Reservation(){
             <h1 className={headTitle.head}>방문수령 예약</h1>
             <p id={styles.reservationNotice}>
             <br></br>
-            성함 :&nbsp;&nbsp;
-            <input id="user_name" name="user_name" type="text" required></input>
-            <br></br>
-            연락처 : &nbsp;&nbsp;
-            <select id="mobile1" className={userForm.userMobile}>
-                                    <option value="010">010</option>
-                                    <option value="011">011</option>
-                                    <option value="016">016</option>
-                                    <option value="017">017</option>
-                                    <option value="018">018</option>
-                                    <option value="019">019</option>
-                                </select>
-                                &nbsp;-&nbsp;<input className={userForm.userMobile} id="mobile2" type="text" maxLength="4" pattern="\d*"></input>
-                                &nbsp;-&nbsp;<input className={userForm.userMobile} id="mobile3" type="text" maxLength="4" pattern="\d*"></input>
-            <br></br>
-            이메일 : &nbsp;&nbsp;
-            <input id="user_email" name="user_email" type="text" required></input>
-            <br></br>
-            <br></br>
-            <span>방문 가능 한 날짜와 시간을 선택해주세요</span>
-            <DatePicker
-            locale={ko}
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-            withPortal
-            customInput={<ExampleCustomInput />}
-            dateFormat="yyyy년 MM월 dd일 H시 mm분"
-            minDate={new Date()}
-            // popperPlacement="auto"
-            showTimeSelect
-            timeFormat=" H시 mm분"
-            timeIntervals={15}
-            filterDate={isWeekday}
-            minTime={minTime}
-            maxTime={maxTime}
-            excludeTimes={[
-                setHours(setMinutes(new Date(), 45), 9),
-                setHours(setMinutes(new Date(), 0), 10),
-                setHours(setMinutes(new Date(), 15), 10),
-                setHours(setMinutes(new Date(), 30), 10),
-                setHours(setMinutes(new Date(), 45), 10),
-                setHours(setMinutes(new Date(), 0), 11),
-                setHours(setMinutes(new Date(), 15), 11),
-                setHours(setMinutes(new Date(), 30), 11),
-                setHours(setMinutes(new Date(), 45), 11),
-                setHours(setMinutes(new Date(), 0), 12),
-                setHours(setMinutes(new Date(), 15), 12),
-                setHours(setMinutes(new Date(), 30), 12),
-                setHours(setMinutes(new Date(), 45), 12),
-                setHours(setMinutes(new Date(), 30), 13),
-                setHours(setMinutes(new Date(), 45), 13),
-                setHours(setMinutes(new Date(), 0), 14),
-                setHours(setMinutes(new Date(), 15), 14),
-                setHours(setMinutes(new Date(), 30), 14),
-                setHours(setMinutes(new Date(), 45), 14),
-                setHours(setMinutes(new Date(), 0), 15),
-                setHours(setMinutes(new Date(), 15), 15),
-                setHours(setMinutes(new Date(), 30), 15),
-                setHours(setMinutes(new Date(), 45), 15),
-                setHours(setMinutes(new Date(), 0), 16),
-                setHours(setMinutes(new Date(), 15), 16),
-                setHours(setMinutes(new Date(), 30), 16),
-                setHours(setMinutes(new Date(), 45), 16),
-                setHours(setMinutes(new Date(), 0), 17)
-            ]}
-            />
-            <span>10분이상 지각 시 담당자가 기다리지 않을 수 있습니다. 방문에 유의해 주세요🙂</span>
-            <br></br>
-            <span>방문 수령 변경 예약은 카카오톡 채널로 문의 바랍니다!</span>
-            <br></br>
+            <form className={styles.reservationForm}>
+                <label>
+                    <span>성함&nbsp;&nbsp;&nbsp;</span>
+                    <input id="user_name" name="user_name" type="text" required></input>
+                </label>
+                <hr></hr>
+                <label>
+                    <span>연락처</span>
+                    <input id="user_num" name="user_num" type="text" required></input>
+                    {/* <select id="mobile1" className={userForm.userMobile}>
+                                            <option value="010">010</option>
+                                            <option value="011">011</option>
+                                            <option value="016">016</option>
+                                            <option value="017">017</option>
+                                            <option value="018">018</option>
+                                            <option value="019">019</option>
+                                        </select>
+                                        &nbsp;-&nbsp;<input className={userForm.userMobile} id="mobile2" type="text" maxLength="4" pattern="\d*"></input>
+                                        &nbsp;-&nbsp;<input className={userForm.userMobile} id="mobile3" type="text" maxLength="4" pattern="\d*"></input> */}
+                </label>
+                <hr></hr>
+                <label>
+                    <span>이메일</span>
+                    <input id="user_email" name="user_email" type="text" required></input>
+                </label>
+                <hr></hr>
+                <p>방문 가능 한 날짜와 시간을 선택해주세요</p>
+                <DatePicker
+                locale={ko}
+                selected={startDate}
+                onChange={date => setStartDate(date)}
+                withPortal
+                customInput={<ExampleCustomInput />}
+                dateFormat="yyyy년 MM월 dd일 H시 mm분"
+                minDate={new Date()}
+                // popperPlacement="auto"
+                showTimeSelect
+                timeFormat=" H시 mm분"
+                timeIntervals={15}
+                filterDate={isWeekday}
+                minTime={minTime}
+                maxTime={maxTime}
+                excludeTimes={[
+                    setHours(setMinutes(new Date(), 45), 9),
+                    setHours(setMinutes(new Date(), 0), 10),
+                    setHours(setMinutes(new Date(), 15), 10),
+                    setHours(setMinutes(new Date(), 30), 10),
+                    setHours(setMinutes(new Date(), 45), 10),
+                    setHours(setMinutes(new Date(), 0), 11),
+                    setHours(setMinutes(new Date(), 15), 11),
+                    setHours(setMinutes(new Date(), 30), 11),
+                    setHours(setMinutes(new Date(), 45), 11),
+                    setHours(setMinutes(new Date(), 0), 12),
+                    setHours(setMinutes(new Date(), 15), 12),
+                    setHours(setMinutes(new Date(), 30), 12),
+                    setHours(setMinutes(new Date(), 45), 12),
+                    setHours(setMinutes(new Date(), 30), 13),
+                    setHours(setMinutes(new Date(), 45), 13),
+                    setHours(setMinutes(new Date(), 0), 14),
+                    setHours(setMinutes(new Date(), 15), 14),
+                    setHours(setMinutes(new Date(), 30), 14),
+                    setHours(setMinutes(new Date(), 45), 14),
+                    setHours(setMinutes(new Date(), 0), 15),
+                    setHours(setMinutes(new Date(), 15), 15),
+                    setHours(setMinutes(new Date(), 30), 15),
+                    setHours(setMinutes(new Date(), 45), 15),
+                    setHours(setMinutes(new Date(), 0), 16),
+                    setHours(setMinutes(new Date(), 15), 16),
+                    setHours(setMinutes(new Date(), 30), 16),
+                    setHours(setMinutes(new Date(), 45), 16),
+                    setHours(setMinutes(new Date(), 0), 17)
+                ]}
+                />
+            </form>
+            <p>10분이상 지각 시 담당자가 기다리지 않을 수 있습니다.<br></br>방문에 유의해 주세요🙂
+            <br></br>방문 수령 변경 예약은 카카오톡 채널로 문의 바랍니다!
+            </p>
             <button id={styles.reservationFinBnt}>예약하기</button>
             <br></br>
                 <a href="https://pf.kakao.com/_JNgis/chat"
