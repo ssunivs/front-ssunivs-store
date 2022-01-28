663c1d99b7df6779dd4dae40a18bbfba9c5e3633
seyxxn <lucy9480@gmail.com>
2022-01-28 16:43:36 +0900

    coupon tag(임시사진으로)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,36 @@ import main_ssua1 from "assets/main_ssua1.png";
 import main_ssua2 from "assets/main_ssua2.png";
 import main_ssua3 from "assets/main_ssua3.png";
 import coupon_tag from "assets/coupon_tag.png";
+// import coupon_tag_open from "assets/coupon_tag_open.png";
+import coupon_tag2 from "assets/fix_ssua.png"; //임시
+import { useState } from "react";
 // import Modal from "components/HomeModal";
-
+// href='javascript:void(0);'
+// onclick="return false;"
 function Home(){
+    const [open, setOpen] = useState(false);
+    const onTagClick = () =>{
+        setOpen((current) => !current);
+    }
     return (
         <div>
             {/* <Modal /> */}
             <div>
+                <a
+                // href="https://www.ssunivs.com/2021/09/19/event-coupon/"
+                // href="javascript:"
+                href={open? "#!" : "https://www.ssunivs.com/2021/09/19/event-coupon/"}
+                target={open? "_self" : "_blank"}
+                rel="noreferrer"
+                >
                 <img
                 title="쿠폰 발급 하러 가기"
                 className={styles.couponTag}
-                src={coupon_tag}
-                alt="coupon_tag">
-                </img>
+                src={open? coupon_tag2 : coupon_tag} //임시
+                alt="coupon_tag"
+                onClick={onTagClick}
+                />
+                </a>
             </div>
             <div  id={styles.home}>
                 <img 
