1b000b03eba44a61cdc2d878dad80b7f1d85d707
bomin Park <qhals2001@gmail.com>
2022-01-28 16:57:00 +0900

    contact css 수정

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -32,19 +32,22 @@ function Contact(){
                     <br></br><br></br>
 
                     <label className={styles.contactForm}>
-                    <textarea className={styles.contactForm} name="contace_contents" required
-                    placeHolder="내용을 입력해주세요." cols="50" rows="5"></textarea>
+                    내용<br></br><br></br>
+                    <textarea className={styles.contactForm} name="contact_contents" required cols="173" rows="10"></textarea>
                     </label>
                     <br></br><br></br>
+                    <hr id={styles.contactLine2}></hr>
+                    <br></br>
                     <input className={styles.contactSend}
-                    type="submit" value="보내기"></input>
+                    type="submit" value="Send"></input>
+                    <br></br>
             </form>
             <hr></hr>
 
             <iframe title="map" id={mapStyles.map}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1321.3384205393681!2d126.95332677111733!3d37.49698945893716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9f7df8977be9%3A0xd1c7d78efd30b86e!2z7ISc7Jq47Yq567OE7IucIOuPmeyekeq1rCDsg4Hrj4TroZw1Neq4uCA4!5e0!3m2!1sko!2skr!4v1642742362939!5m2!1sko!2skr"
             allowfullscreen=""
-            loading="lazy"></iframe>  
+            loading="lazy"></iframe> 
         </div>
     );
 }
