241558fea0314beaabbb4c9b0dda051e7023110d
hjch0211 <gkswocks123@gmail.com>
2022-02-21 20:18:56 +0900

    Update sociaol links animation

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -13,51 +13,46 @@ function SocialLinks( { size, color } ){ //햄버거 메뉴, 푸터에 들어갑
     return (
         <div className={styles.socialIcons}>
             <a title="KakaoTalk"
-                className={styles.icon}
                 href="https://pf.kakao.com/_JNgis/chat"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                        color: color}}><RiKakaoTalkFill /></a>
+                        color: color}}><RiKakaoTalkFill className={styles.icon}/></a>
 
             <a title="Instagram"
-                className={styles.icon}
                 href="https://www.instagram.com/snvs.official/"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                    color: color}}><RiInstagramLine /></a>
+                    color: color}}><RiInstagramLine className={styles.icon}/></a>
 
             <a title="Youtube"
-                className={styles.icon}
                 href="https://www.youtube.com/channel/UC1xb52mAFAng2sQFaBhZ-KQ"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                    color: color}}><RiYoutubeFill /></a>
+                    color: color}}><RiYoutubeFill className={styles.icon}/></a>
 
             <a title="Twitter"
-                className={styles.icon}
                 href="https://nitter.net/ssunivs/status/1433695440625487873#m"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                    color: color}}><RiTwitterFill /></a>
+                    color: color}}><RiTwitterFill className={styles.icon}/></a>
 
             <a title="GitHub"
-                className={styles.icon}
                 href="https://github.com/ssunivs"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                    color: color}}><RiGithubFill /></a>
+                    color: color}}><RiGithubFill className={styles.icon}/></a>
             
             <a title="email"
                 href="mailto:ssua@ssunivs.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
-                    color: color}}><HiOutlineMail /></a>
+                    color: color}}><HiOutlineMail className={styles.icon}/></a>
 
         </div>
     );
