37a038f313d53402a63a92d2a45525d6d6038496
seyxxn <lucy9480@gmail.com>
2022-02-23 18:02:04 +0900

    SocialLinks css

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,6 +5,9 @@ import {
     RiTwitterFill,
     RiGithubFill} from "react-icons/ri";
 
+import {CgLoadbar} from "react-icons/cg";
+import {SiNotion} from "react-icons/si";
+
 import { HiOutlineMail } from "react-icons/hi";
 
 import styles from "components/SocialLinks.module.css";
@@ -18,42 +21,49 @@ function SocialLinks( { size, color } ){ //햄버거 메뉴, 푸터에 들어갑
                 rel="noopener noreferrer"
                 style={{fontSize : size,
                         color: color}}><RiKakaoTalkFill className={styles.icon}/></a>
-
-            <a title="Instagram"
-                href="https://www.instagram.com/snvs.official/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{fontSize : size,
-                    color: color}}><RiInstagramLine className={styles.icon}/></a>
-
-            <a title="Youtube"
-                href="https://www.youtube.com/channel/UC1xb52mAFAng2sQFaBhZ-KQ"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{fontSize : size,
-                    color: color}}><RiYoutubeFill className={styles.icon}/></a>
-
-            <a title="Twitter"
-                href="https://nitter.net/ssunivs/status/1433695440625487873#m"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{fontSize : size,
-                    color: color}}><RiTwitterFill className={styles.icon}/></a>
-
-            <a title="GitHub"
-                href="https://github.com/ssunivs"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{fontSize : size,
-                    color: color}}><RiGithubFill className={styles.icon}/></a>
-            
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
             <a title="email"
                 href="mailto:ssua@ssunivs.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{fontSize : size,
                     color: color}}><HiOutlineMail className={styles.icon}/></a>
-
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
+            <a title="Instagram"
+                href="https://www.instagram.com/snvs.official/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{fontSize : size,
+                    color: color}}><RiInstagramLine className={styles.icon}/></a>
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
+            <a title="Twitter"
+                href="https://nitter.net/ssunivs/status/1433695440625487873#m"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{fontSize : size,
+                    color: color}}><RiTwitterFill className={styles.icon}/></a>
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
+            <a title="Youtube"
+                href="https://www.youtube.com/channel/UC1xb52mAFAng2sQFaBhZ-KQ"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{fontSize : size,
+                    color: color}}><RiYoutubeFill className={styles.icon}/></a>
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
+            <a  title="Notion"
+                href="https://www.notion.so/ssuniverse/SSUNIVERSE-7373f0cd163d4d2ca0b1574389441727"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{fontSize : size,
+                    color: color}}><SiNotion className={styles.icon} size="25"/></a>
+            <CgLoadbar  className={styles.iconsConnection} size="15"/>
+            <a  title="GitHub"
+                href="https://github.com/ssunivs"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{fontSize : size,
+                    color: color}}><RiGithubFill className={styles.icon}/></a>
+        
         </div>
     );
 }
