f4365a13ae4556f0abab96853e08a3cb7f63a1ba
seyxxn <lucy9480@gmail.com>
2022-02-28 22:11:27 +0900

    aboutssua img

--- src/pages/AboutSsua.js
+++ src/pages/AboutSsua.js
@@ -1,5 +1,7 @@
 import NavBar from "components/NavBar.js";
 import headTitle from "pages/headTitle.module.css";
+import about_ssua_img from "assets/about_ssua_img.png";
+import styles from "pages/AboutSsua.module.css";
 
 function AboutSsua(){
     return (
@@ -7,6 +9,11 @@ function AboutSsua(){
             <br></br>
             <NavBar />
             <h1 className={headTitle.head}>슈아(𝐒𝐒𝐔-𝐀)를 소개합니다 !</h1>
+            <img 
+                className={styles.aboutSsuaImg}
+                src={about_ssua_img}
+                alt="about_ssua_img" 
+                />
         </div>
     );
 }
