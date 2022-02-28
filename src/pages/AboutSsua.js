7d1207142b9f0c927aff7414a398c8c316ddc590
seyxxn <lucy9480@gmail.com>
2022-02-28 21:44:17 +0900

    css 수정

--- src/pages/AboutSsua.js
+++ src/pages/AboutSsua.js
@@ -6,7 +6,7 @@ function AboutSsua(){
         <div>
             <br></br>
             <NavBar />
-            <h1 className={headTitle.head}>슈아(ssu-a)를 소개합니다 !</h1>
+            <h1 className={headTitle.head}>슈아(𝐒𝐒𝐔-𝐀)를 소개합니다 !</h1>
         </div>
     );
 }
