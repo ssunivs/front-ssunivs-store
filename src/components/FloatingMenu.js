f3faabd3dc94fd475d605eaaba06ab8118ee8c8b
seyxxn <lucy9480@gmail.com>
2022-03-06 14:39:36 +0900

    scroll to top btn

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -1,51 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "components/FloatingMenu.module.css";
 import fixSsuaIcon from "../assets/ssua_floating_btn.png";
 import {GrMail,GrInstagram} from "react-icons/gr"
 import {RiKakaoTalkFill} from "react-icons/ri"
 import {AiFillShopping} from "react-icons/ai";
+import {BiChevronUpCircle} from "react-icons/bi";
 
 function FloatingMenu() {
     const [open, setOpen] = useState(false);
     const onFloatingMenuClick = () =>{
         setOpen((current) => !current);
     }
+    const [ScrollY, setScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
+    const handleFollow = () => {
+    setScrollY(window.pageYOffset); // window 스크롤 값을 ScrollY에 저장
+    }
+
+    useEffect(() => {
+    }, [ScrollY])
+
+    useEffect(() => {
+    const watch = () => {
+        window.addEventListener('scroll', handleFollow);
+    }
+    watch(); // addEventListener 함수를 실행
+    return () => {
+      window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
+        }
+    })
+
+    const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
+            setScrollY(0);  // ScrollY 의 값을 초기화
+    }
+
     return (
-        <div className={styles.floatingMenu}>
-            <ul className={open? styles.openDetailMenu : styles.floatingDetailMenu}>
-                    <a title="Shop"
-                    href="https://www.ssunivs.com/"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                        <li id={styles.floatingDetailIcons}><AiFillShopping size="35" color="#04488C"/></li>
-                    </a>
-                    <a title="KakaoTalk"
-                    href="https://pf.kakao.com/_JNgis/chat"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                        <li id={styles.floatingDetailIcons}><RiKakaoTalkFill size="35" color="#04488C"/></li>
-                    </a>
-                    <a title="Instagram"
-                    href="https://www.instagram.com/snvs.official/"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                        <li id={styles.floatingDetailIcons}><GrInstagram size="35" color="#04488C"/></li>
-                    </a>
-                    <a title="Mail"
-                    href="mailto:ssua@ssunivs.com"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                        <li id={styles.floatingDetailIcons}><GrMail size="35" color="#04488C"/></li>
-                    </a>
-            </ul>
-                <img 
-                className={styles.fixSsuaIcon}
-                onClick={onFloatingMenuClick}
-                src={fixSsuaIcon}
-                alt="슈아고정버튼" 
-                />
-        </div> 
+        <div>
+            <div className={open?  styles.scrollToTopHidden : styles.scrollToTop}
+                onClick={handleTop}>
+                <BiChevronUpCircle size="35" color="#04488C"/>
+            </div>
+            <div className={styles.floatingMenu}>
+                <ul className={open? styles.openDetailMenu : styles.floatingDetailMenu}>
+                        <a title="Shop"
+                        href="https://www.ssunivs.com/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <li id={styles.floatingDetailIcons}><AiFillShopping size="35" color="#04488C"/></li>
+                        </a>
+                        <a title="KakaoTalk"
+                        href="https://pf.kakao.com/_JNgis/chat"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <li id={styles.floatingDetailIcons}><RiKakaoTalkFill size="35" color="#04488C"/></li>
+                        </a>
+                        <a title="Instagram"
+                        href="https://www.instagram.com/snvs.official/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <li id={styles.floatingDetailIcons}><GrInstagram size="35" color="#04488C"/></li>
+                        </a>
+                        <a title="Mail"
+                        href="mailto:ssua@ssunivs.com"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <li id={styles.floatingDetailIcons}><GrMail size="35" color="#04488C"/></li>
+                        </a>
+                </ul>
+                    <img 
+                    className={styles.fixSsuaIcon}
+                    onClick={onFloatingMenuClick}
+                    src={fixSsuaIcon}
+                    alt="슈아고정버튼" 
+                    />
+            </div> 
+        </div>
     );
 }
 
